8be9a386cfff24a15d02c02df988af88a1fa45fd
bsamworthdev <bsamworthdev@outlook.com>
2021-06-03 16:31:22 +0100

    Initial commit

---
+++ database/migrations/2021_06_03_110533_create_employees_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 255);
+            $table->string('last_name', 255);
+            $table->unsignedBigInteger('company');
+            $table->string('email', 255);
+            $table->string('phone', 20);
+            $table->timestamps();
+
+            //Set foreign key
+            $table->foreign('company')->references('id')->on('companies')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
